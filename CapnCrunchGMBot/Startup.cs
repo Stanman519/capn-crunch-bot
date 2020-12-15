8ad862fba83340fda74e8bb6c29e741472f2168b
RyanStanley <Ryan.Stanley@dcsg.com>
2020-12-15 10:58:05 -0500

    turned on https redirect

--- CapnCrunchGMBot/Startup.cs
+++ CapnCrunchGMBot/Startup.cs
@@ -21,6 +21,7 @@ namespace CapnCrunchGMBot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpsRedirection(opt => opt.HttpsPort = 443);
             services.AddControllers();
             services.AddSwaggerGen();
             services.AddSingleton(RestClient.For<IDeadCapApi>("https://ryan-passion-project.apps.vn01.pcf.dcsg.com"));
