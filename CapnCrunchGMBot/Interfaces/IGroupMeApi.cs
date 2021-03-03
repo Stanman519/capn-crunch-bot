8845ce216df668b04dee00044067e8c9327d0873
RyanStanley <Ryan.Stanley@dcsg.com>
2021-03-03 09:49:48 -0500

    token swap

--- CapnCrunchGMBot/Interfaces/IGroupMeApi.cs
+++ CapnCrunchGMBot/Interfaces/IGroupMeApi.cs
@@ -6,10 +6,10 @@ namespace CapnCrunchGMBot.Interfaces
 {
     public interface IGroupMeApi
     {
-       [Post("v3/bots/post")]
+        [Post("v3/bots/post")]
         Task SendMessage([Body]Message message);
         
-        [Get("v3/groups/59795205?token=TNkXkjDPQ7jRs8r0hZKpfMRBaqWXk6AOyuKywGIE")]
+        [Get("v3/groups/59795205?token=YjMEBw8kwXkJMKxDz2nd2o0iG9aC1GG4NjD9O1ih")]
         Task<GroupParent> GetMemberIds();
         
         //TODO: pinging the bot for something
