54d383eca408aa8ee8e5a004f8cbde6d0df23986
RyanStanley <Ryan.Stanley@dcsg.com>
2021-05-05 15:43:22 -0400

    constructor issue

--- CapnCrunchGMBot/Models/ErrorMessage.cs
+++ CapnCrunchGMBot/Models/ErrorMessage.cs
@@ -3,10 +3,5 @@ namespace CapnCrunchGMBot.Models
     public class ErrorMessage
     {
         public string Message { get; set; }
-
-        public ErrorMessage(string msg)
-        {
-            Message = msg;
-        }
     }
 }
